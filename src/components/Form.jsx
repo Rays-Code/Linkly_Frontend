c7207c5dd6cc15ca2bf140f5db48684a4a1b7840
Dipraj Ray <raydipraj1234@gmail.com>
2025-08-30 22:54:38 +0530

    resize link size

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -11,7 +11,7 @@ const Form = () => {
         const res = await axios.post(`${import.meta.env.VITE_BASE_URL}/api/shorten`, {
             longURL: longURL
         })
-        setShortURL(res.data.shortURL.slice(12, 28));
+        setShortURL(res.data.shortURL.slice(12, 29));
     }
 
   return (
