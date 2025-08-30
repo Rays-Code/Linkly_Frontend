0318a3fad8b95b3f3878e35d2314e403e6e9c153
Dipraj Ray <raydipraj1234@gmail.com>
2025-08-30 23:28:57 +0530

    added downtime alert

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Title from '../components/Title'
 import Form from '../components/Form'
-import Alert from '../components/alert'
+import Alert from '../components/Alert'
 
 
 
